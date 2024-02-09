7eb59ae7369533bd24139d32d3ba447c58a3b122
karlmorisset <karl.morisset.register@gmail.com>
2024-02-09 15:07:18 +0100

    initial setup ok

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+      <h1 className="text-3xl font-bold underline">
+        Hello SnapGram!
+      </h1>
+    </main>
+  )
+}
+
+export default App
