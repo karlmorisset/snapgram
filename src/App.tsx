184a26ad594b87699f7dc1f022ecbe129a997816
karlmorisset <karl.morisset.register@gmail.com>
2024-02-09 15:21:43 +0100

    Routage en place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import {Routes, Route} from 'react-router-dom'
+import SigninForm from './_auth/forms/SigninForm copy'
+import SignupForm from './_auth/forms/SignupForm'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/pages/RootLayout'
 import './globals.css'
 
 const App = () => {
   return (
     <main className='flex h-screen'>
-      <h1 className="text-3xl font-bold underline">
-        Hello SnapGram!
-      </h1>
+      <Routes>
+        {/* public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom/client";
+import {BrowserRouter} from 'react-router-dom'
 import App from "./App";
 
 
 const root = document.getElementById('root')
 
-ReactDOM.createRoot(root!).render(<App />);
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
